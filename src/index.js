debef80b8f306bb784bdd3ed5ee9433301b5dff4
Ben Lynch <bdlynch7@gmail.com>
2023-08-24 14:08:46 -0500

    added saga for GET route, finished index setup

--- src/index.js
+++ src/index.js
@@ -8,9 +8,30 @@ import logger from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
 import { takeLatest, put } from 'redux-saga/effects'
 
+
+function* rootSaga() {
+  yield takeLatest('GET_GIFS', fetchGifList)
+}
+
+function* fetchGifList() {
+  const gifList = yield axios.get('/api/favorite')
+  yield put ({ type: 'SET_GIF_LIST', payload: gifList.data })
+}
+
+const store = createStore(
+  combineReducers({ }),
+  applyMiddleware( sagaMiddleware, logger )
+)
+
+const sagaMiddleware = createSagaMiddleware()
+
+saga.Middleware.run( rootSaga )
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+      <Provider store={store}>
         <App />
+      </Provider>
     </React.StrictMode>
 );
