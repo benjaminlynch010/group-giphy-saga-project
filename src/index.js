708106cadf8c87a7ae23f557dec274ed0c0be7da
Ben Lynch <bdlynch7@gmail.com>
2023-08-24 10:59:13 -0500

    imported dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
+import axios from 'axios'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import logger from 'redux-logger'
+import createSagaMiddleware from 'redux-saga'
+import { takeLatest, put } from 'redux-saga/effects'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
