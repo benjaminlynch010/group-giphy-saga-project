c292758283fee0506cdf54a0044e0d59c4a26292
fraol <phrolaye@gmail.com>
2023-08-25 10:10:13 -0500

    index and search

--- server/routes/category.router.js
+++ server/routes/category.router.js
@@ -1,36 +1,19 @@
 const express = require('express');
 const pool = require('../modules/pool');
-const axios = require('axios')
 const router = express.Router();
 
-// router.get('/', (req, res) => {
-//   // return all categories
-//   const queryText = `SELECT * FROM category ORDER BY name ASC`;
-//   pool
-//     .query(queryText)
-//     .then((result) => {
-//       res.send(result.rows);
-//     })
-//     .catch((error) => {
-//       console.log(`Error on query ${error}`);
-//       res.sendStatus(500);
-//     });
-// });
-
 router.get('/', (req, res) => {
-    console.log("My giphy api key:", process.env.GIPHY_API_KEY)
-
-    const apiKey = process.env.GIPHY_API_KEY
-
-    axios.get(`http://api.giphy.com/v1/gifs/search?api_key=jSVa0O9ielMoIqs7AXdoDYqI5mWksCGT`)
-    .then((response) => {
-        console.log("Success retrieving trending")
-        res.send(response.data)
+  // return all categories
+  const queryText = `SELECT * FROM category ORDER BY name ASC`;
+  pool
+    .query(queryText)
+    .then((result) => {
+      res.send(result.rows);
     })
     .catch((error) => {
-        console.log("Error retrieving trending:", error)
-        res.sendStatus(500)
-    })
-})
+      console.log(`Error on query ${error}`);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -6,42 +6,101 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import logger from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
-import { takeLatest, put } from 'redux-saga/effects'
+import { takeEvery, put } from 'redux-saga/effects'
 
 
-function* rootSaga() {
-  yield takeLatest('GET_GIFS', fetchGifList)
+function* sendSearch (action) {
+  // 3. send search takes the dispatch from 'SAGA_FETCH_SEARCH', and supplying the function with the search term.
+  try{
+      // 4. assigns the term to the searchQuery variable.
+      const searchQuery = action.payload;
+      // 5. set params with the variable of searchQuery as an object.
+      const response = yield  axios({
+          method:  'GET',
+          url: '/api/favorite/search',
+          params: {searchQuery}
+      })
+      // Send GIF data to results reducer
+   yield put ({
+      // 9. this sets the state of the reducer with the corresponding type.
+      type: 'SET_GIF_LIST',
+      payload: response
+      })
+  }catch (error){
+      console.log(`sendSearch broke POST saga index`, error);
+  }
 }
 
-function* fetchGifList() {
-  const gifList = yield axios.get('/api/category')
-  yield put ({ type: 'SET_GIF_LIST', payload: gifList.data})
+function* fetchGifs () {
+  try{
+      const response = yield  axios({
+          method:  'GET',
+          url: '/api/favorite'
+      })
+   yield put ({
+      type: 'SET_STATE',
+      payload: response.data
+      })
+  }catch (error){
+      console.log(`fetch gif broke GET saga index`, error);
+  }
 }
+
+
+
+const favorites = (state = [], action) => {
+  switch (action.type) {
+      case 'SET_STATE':
+          return action.payload;
+      default:
+          return state;
+  }
+}
+
+// function* rootSaga() {
+//   yield takeLatest('GET_GIFS', fetchGifList)
+// }
+
+// function* fetchGifList() {
+//   const gifList = yield axios.get('/api/category')
+//   yield put ({ type: 'SET_GIF_LIST', payload: gifList.data})
+// }
 
 const gifReducer = ( state = [], action ) => {
   switch(action.type) {
     case 'SET_GIF_LIST':
-      return action.payload
+      return action.payload.data
     default: 
       return state
   }
 }
 
+// Creates generator 
+function* rootSaga(){
+  yield  takeEvery('SAGA_FETCH_GIFS', fetchGifs)
+  // 2. sendSearch listens for 'SAGA_FETCH_SEARCH' and then sendSearch gets called
+  yield  takeEvery('SAGA_FETCH_SEARCH', sendSearch)
+}
+
+
 const sagaMiddleware = createSagaMiddleware()
 
-const store = createStore(
+const storeInstance = createStore(
   combineReducers({ 
     gifReducer,
+    favorites
   }),
   applyMiddleware( sagaMiddleware, logger )
 )
 
-sagaMiddleware.run( rootSaga )
+sagaMiddleware.run(rootSaga)
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-      <Provider store={store}>
+      <Provider store={storeInstance}>
         <App />
       </Provider>
     </React.StrictMode>
